400f9b6b94211058f01f5c3ec0e07631a6c4a7ed
Yuki Takasu <t.yuki0370@gmail.com>
2022-01-31 01:05:59 +0900

    heroku bug fix

--- app/app/settings.py
+++ app/app/settings.py
@@ -70,7 +70,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8080',
     'http://127.0.0.1:8080',
-    'https://social-drawing-guessing.netlify.app/'
+    'https://social-drawing-guessing.netlify.app'
 )
 
 CORS_ALLOW_HEADERS = (
