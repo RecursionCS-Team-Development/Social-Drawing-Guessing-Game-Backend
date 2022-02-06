d17ebb0fccb839c381476d6fafdfe4803125a043
Yuki Takasu <t.yuki0370@gmail.com>
2022-02-06 12:45:27 +0900

    chat router

--- app/app/asgi.py
+++ app/app/asgi.py
@@ -12,7 +12,7 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import chat.routing
+from ..chat import routing
 
 import django
 
@@ -23,6 +23,6 @@ django.setup()
 application = ProtocolTypeRouter({
   'http': get_asgi_application(),
   'websocket': AuthMiddlewareStack(
-    URLRouter(chat.routing.websocket_urlpatterns)
+    URLRouter(routing.websocket_urlpatterns)
   )
 })
