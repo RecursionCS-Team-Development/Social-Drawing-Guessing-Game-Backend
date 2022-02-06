3377077221fbe1968bcec2ec924e4637187c2f69
Yuki Takasu <t.yuki0370@gmail.com>
2022-02-06 11:52:12 +0900

    chatlog追加

--- app/drawing/urls.py
+++ app/drawing/urls.py
@@ -2,7 +2,8 @@ from rest_framework import routers
 from django.urls import path
 from . import views
 
-router = routers.DefaultRouter()
+# router = routers.DefaultRouter()
+# router.register(r'chatlog', views.ChatLogViewSet)
 
 room_list = views.RoomViewSet.as_view({
     'get': 'list',
@@ -14,8 +15,14 @@ picture_list = views.PictureViewSet.as_view({
     'post': 'create'
 })
 
+chat_list = views.ChatLogViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
 urlpatterns = [
     path('', views.IndexView, name='index'),
     path('rooms/', room_list, name='room-list'),
+    path('chat/', chat_list, name='chat'),
     path('pictures/', picture_list, name='picture-list'),
 ]
