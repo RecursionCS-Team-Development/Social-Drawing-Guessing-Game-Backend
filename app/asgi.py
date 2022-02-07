2e35b344be59c413baa33fc487186f7fd36e59c3
Masayuki <soy2.san@gmail.com>
2022-02-07 23:01:27 +0900

    test asgi

--- app/asgi.py
+++ app/asgi.py
@@ -13,16 +13,17 @@ import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import chat.routing
+
+django_asgi_app = get_asgi_application()
 
 import django
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 django.setup()
 
-# ProtocolTypeRouterを使用してアクセスしてきたスキーマをcheckしてhttp, websocket(ws, wss)を分ける
 application = ProtocolTypeRouter({
-  'http': get_asgi_application(),
+  'http': django_asgi_app,
   'websocket': AuthMiddlewareStack(
     URLRouter(chat.routing.websocket_urlpatterns)
   )
