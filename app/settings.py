03323b92887272a3dda0f84ef2890b2495331091
Masayuki <soy2.san@gmail.com>
2022-03-02 23:29:59 +0900

    test

--- app/settings.py
+++ app/settings.py
@@ -283,39 +283,4 @@ AUTH_USER_MODEL = 'accounts.User'
 
 # heroku
 django_heroku.settings(locals())
-del DATABASES['default']['OPTIONS']['sslmode']
-
-# Support for X-Request-ID
-
-LOG_REQUEST_ID_HEADER = 'HTTP_X_REQUEST_ID'
-LOG_REQUESTS = True
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'request_id': {
-            '()': 'log_request_id.filters.RequestIDFilter'
-        }
-    },
-    'formatters': {
-        'standard': {
-            'format': '%(levelname)-8s [%(asctime)s] [%(request_id)s] %(name)s: %(message)s'
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'filters': ['request_id'],
-            'formatter': 'standard',
-        },
-    },
-    'loggers': {
-        'log_request_id.middleware': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    }
-}
+# del DATABASES['default']['OPTIONS']['sslmode']
